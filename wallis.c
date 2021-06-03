730a8156881061a88c1773cd5fa62bf42c44c3c3
kishan-blip <84665824+kishan-blip@users.noreply.github.com>
2021-06-04 03:05:38 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -26,11 +26,12 @@ int main(void) {
 float wallis_pi(int n)
 {
   float p = 1.0;
-  for (int i = 1;i < n; i++)
+  for (int i = 1; i < n; i++)
   { 
    float num = 4.0 * i * i;
     p *= num/(num - 1);
   }
+}
                              
   
   
